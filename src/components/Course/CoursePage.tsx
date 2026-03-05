2cfc3566b302a0185574c6f55df8c82f7930ed5b
Hank <han98115@gmail.com>
2026-03-04 19:00:55 -0500

    Update CoursePage.tsx

--- src/components/Course/CoursePage.tsx
+++ src/components/Course/CoursePage.tsx
@@ -142,10 +142,10 @@ const CoursePage: React.FC = () => {
                                         <td className="py-3 font-semibold">Jan. 12</td>
                                         <td className="py-3">
                                             Introduction to Efficient ML
-                                            &nbsp;(<a href="https://youtu.be/amAnp0bzS14" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">[Video]</a>)
+                                            &nbsp;<a href="https://youtu.be/amAnp0bzS14" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">[Video]</a>
                                             <br />
                                             Basics of Deep Learning
-                                            &nbsp;(<a href="https://youtu.be/rm6u0G9SKKA" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">[Video]</a>) 
+                                            &nbsp;<a href="https://youtu.be/rm6u0G9SKKA" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">[Video]</a>
                                         </td>
                                         <td className="py-3 text-blue-600">
                                         <a href="https://colab.research.google.com/drive/1U5-7mX9DuV8_krZR9eMocZoO-kabR0qT" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">Lab 0</a> Out
@@ -176,10 +176,10 @@ const CoursePage: React.FC = () => {
                                         <td className="py-3 font-semibold">Feb. 16</td>
                                         <td className="py-3">
                                             Knowledge Distillation
-                                            &nbsp;(<a href="https://youtu.be/khDgn1rU0s4" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">[Video]</a>)
+                                            &nbsp;<a href="https://youtu.be/khDgn1rU0s4" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">[Video]</a>
                                             <br />
                                             TinyML
-                                            &nbsp;(<a href="https://youtu.be/Vuw6rFj4gn8" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">[Video]</a>)
+                                            &nbsp;<a href="https://youtu.be/Vuw6rFj4gn8" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">[Video]</a>
                                         </td>
                                         <td className="py-3" />
                                     </tr>
@@ -197,7 +197,7 @@ const CoursePage: React.FC = () => {
                                             &nbsp;(Part III &nbsp;<a href="https://youtu.be/sVclQpdWsx8" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">[Video]</a>)
                                             <br />
                                             Efficient LLM Deployment
-                                            &nbsp;(<a href="https://youtu.be/6-bbvFbBAJk" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">[Video]</a>)
+                                            &nbsp;<a href="https://youtu.be/6-bbvFbBAJk" className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">[Video]</a>
                                         </td>
                                         <td className="py-3" />
                                     </tr>
